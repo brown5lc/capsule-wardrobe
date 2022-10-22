f9fb2ccc5595962276cde3601df24a3f783dba7c
brown5lc <liamxxbrown@gmail.com>
2022-10-22 17:08:14 -0400

    added prisma... kinda

--- src/pages/wardrobes.tsx
+++ src/pages/wardrobes.tsx
@@ -1,17 +1,30 @@
 import type { NextPage } from "next";
+import {useState} from 'react';
 import Head from "next/head";
 import styles from "./index.module.css";
 import Navbar from "../navigation/Navbar.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Wardrobes: NextPage = () => {
+    const [message, setMessage] = useState('');
+    const handleChange = event => {
+        setMessage(event.target.value);
+    }
     return(
-        <>
+        <> 
         <Navbar/> 
         <b>This is the wardrobe page</b>
             <div>
                 <button type="button">Create Wardrobe</button>
             </div>
+        <input
+            type="text"
+            id="message"
+            name="message"
+            onChange={handleChange}
+            value={message}
+        />
+
         </>
     )
 };
